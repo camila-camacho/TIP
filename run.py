8b0a4e0a72791244338de0d47d21724a2f30c216
Camila <camachcf@dukes.jmu.edu>
2022-03-26 12:51:07 +0100

    try dep GCP

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from app import app 
+import os 
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=8080)
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT",8080)))
    #app.run(debug=True)
     
